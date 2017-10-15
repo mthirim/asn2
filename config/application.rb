4a80e53e1436fff4a4f72eeda502995d91c8ceb9
mthirim <myathirimay@gmail.com>
2017-10-15 12:16:27 -0700

    update

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.assets.initialize_on_compile = false
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
