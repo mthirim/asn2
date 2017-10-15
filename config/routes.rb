28deda8d68b7e55e4c9c0ef044f2bcadad197890
mthirim <myathirimay@gmail.com>
2017-10-15 14:51:04 -0700

    update

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,20 @@ Rails.application.routes.draw do
 	root 'courses#index'
 
 	get 'courses/histogram'
+	get 'courses/edit'
+	get 'courses/index'
+	get 'courses/show'
+	get 'courses/new'
+
+	get 'enrollments/edit'
+	get 'enrollments/index'
+	get 'enrollments/show'
+	get 'enrollments/new'
+
+	get 'students/edit'
+	get 'students/index'
+	get 'students/show'
+	get 'students/new'
 
   resources :enrollments
   resources :courses
